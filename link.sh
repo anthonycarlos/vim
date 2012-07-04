5ade174fbda70559d955258d324d49a96849f293
Anthony Carlos <aec@Anthony-Carloss-MacBook-Pro.local>
2012-07-04 08:54:54 -0400

    Add shell script to ease use of files.

---
+++ link.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ln -s ~/vim/.gvimrc.after  ~/.gvimrc.after
+ln -s ~/vim/.gvimrc.before ~/.gvimrc.before
+ln -s ~/vim/.vimrc.before  ~/.vimrc.before
+ln -s ~/vim/.vimrc.after   ~/.vimrc.after
